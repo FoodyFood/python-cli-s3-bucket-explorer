d25005c51b51ac4371a6365dc02b3313cda291dd
doonnor <doconnor@numerix.com>
2022-10-15 11:45:14 +0000

    init commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import boto3
+from simple_term_menu import TerminalMenu
+
+
+bucket_name = 'bucket name'
+aws_profile = 'profile name'
+
+
+boto3_session = boto3.session.Session(profile_name=aws_profile)
+s3_client = boto3_session.client('s3')
+
+
+selected_folder="" # Start off with empty string to get top level folder list
+
+
+while True:
+    # Get list of folders in current selected_folder (if empty string, get's top level folders)
+    list_of_objects = s3_client.list_objects(Bucket=bucket_name, Prefix=selected_folder', Delimiter="/")
+
+    # If there are no folders, exit the while loop
+    if 'CommonPrefixes' not in list_of_objects:
+        break
+
+    # Extract the folder paths from the list of objects
+    folder_paths = [x['Prefix'] for x in list_of_objects['CommonPrefixes']]
+
+    # Remove the start of each folder path so we display only the current folders in the menu, not the whole path
+    folders: list = []
+    for folder_path in folder_paths:
+        folders.append(folder_path[len(selected_folder):-1])
+
+    # Display the menu to select the folder
+    folder_menu = TerminalMenu(folders)
+    folder_menu_selection_index = folder_menu.show()
+
+    # Populate the selected_folder variable with the full key seleted
+    selected_folder=folder_paths[folder_menu_selection_index]
+
+
+# Catch in case there were no folders in the bucket
+if selected_folder == "":
+    print("No folders found in bucket.")
+
+
+
+# End of tree reached, can ask user if they would like to download the folder or browse and download single files
+
+
+# Display the list of files in the folder the user selected
+objects_in_selected_folder = s3_client.list_objects_v2(Bucket = bucket_name, Prefix=selected_folder) 
+for object in objects_in_selected_folder['Contents']:
+    print(object['Key'])
+
+
+
