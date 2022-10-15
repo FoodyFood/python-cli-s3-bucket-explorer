bf5a1522b2352e190484811d51aba60c6214eee6
doonnor <doconnor@numerix.com>
2022-10-15 12:51:00 +0000

    Add back button and exit button to the menus

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ bucket_name = '<bucket name>'
 aws_profile = '<aaws profile name>'
 
 
+# Create our boto3 session and client using the desired credentials
 boto3_session = boto3.session.Session(profile_name=aws_profile)
 s3_client = boto3_session.client('s3')
 
 
-selected_folder="" # Start off with empty string to get top level folder list
+# Start off with empty string to get top level folder list
+selected_folder=""
 
 
+# Alow the user to select folders till we run out of folders to select and break
 while True:
     # Get list of folders in current selected_folder (if empty string, get's top level folders)
     list_of_objects = s3_client.list_objects(Bucket=bucket_name, Prefix=selected_folder, Delimiter="/")
@@ -28,13 +31,33 @@ while True:
     folders: list = []
     for folder_path in folder_paths:
         folders.append(folder_path[len(selected_folder):-1])
+    
+    if "/" in selected_folder:
+        folders.append("Back To Previous Folder")
+    folders.append("Exit Folder Selection")
 
     # Display the menu to select the folder
     folder_menu = TerminalMenu(folders)
     folder_menu_selection_index = folder_menu.show()
 
-    # Populate the selected_folder variable with the full key seleted
-    selected_folder=folder_paths[folder_menu_selection_index]
+    # If user wants to navigate up a folder, remove the last flder name from the path and reapend the "/" if needed
+    if folders[folder_menu_selection_index] == "Back To Previous Folder":
+        selected_folder = ("/".join(selected_folder.split("/")[:-2]))
+        if selected_folder != "":
+                selected_folder = selected_folder + "/"
+
+    # If the user wants to exit the downoad menu
+    elif folders[folder_menu_selection_index] == "Exit Folder Selection":
+        exit()
+
+    # Otherwise, we populate the selected_folder variable with the full key seleted
+    else:
+        selected_folder=folder_paths[folder_menu_selection_index]
+
+
+
+# Print the full path of the older the user landed on
+print(f"\nSelected Folder: {selected_folder}\n")
 
 
 # Catch in case there were no folders in the bucket
@@ -42,26 +65,41 @@ if selected_folder == "":
     print("No folders found in bucket.")
 
 
+# End of tree reached, ask the user if they would like to download the folder or browse and download single files
+print("No more folder levels, would you like to:\n")
+download_options=["Download The Complete Folder", "Download Single File From The Folder", "Exit Download Menu"]
+download_menu = TerminalMenu(download_options)
+download_menu_selection_index = download_menu.show()
 
-# End of tree reached, can ask user if they would like to download the folder or browse and download single files
+
+# In case the user jsut wanted to browse files, not download them
+if(download_options[download_menu_selection_index] == "Exit Download Menu"):
+    print("Exiting Download Menu")
+    exit()
 
 
 # Get the list of files in the folder the user selected
 files_in_selected_folder = s3_client.list_objects_v2(Bucket = bucket_name, Prefix=selected_folder) 
 
-# Remove the path prefix from each file
-files: list = []
-for file in files_in_selected_folder['Contents']:
-    files.append(file['Key'][len(selected_folder):])
 
-# Display the menu to select the folder
-file_menu = TerminalMenu(files)
-file_menu_selection_index = file_menu.show()
+# If user wants to download a single file, we displayy a menu to select the file
+if(download_options[download_menu_selection_index] == "Download Single File From The Folder"):
+    # Remove the path prefix from each file
+    files: list = []
+    for file in files_in_selected_folder['Contents']:
+        files.append(file['Key'][len(selected_folder):])
 
-selected_file=files_in_selected_folder['Contents'][file_menu_selection_index]['Key']
+    # Display the menu to select the file
+    file_menu = TerminalMenu(files)
+    file_menu_selection_index = file_menu.show()
+    selected_file=files_in_selected_folder['Contents'][file_menu_selection_index]['Key']
 
-print(selected_file)
+    # Download the selected file
+    print("Downloading: " + selected_file)
+    s3_client.download_file(bucket_name, selected_file, selected_file[len(selected_folder):])
 
-s3_client.download_file(bucket_name, selected_file, selected_file[len(selected_folder):])
+
+# If the user wants to download the whole folder, we just loop over the files one by one and download them
+
 
 
